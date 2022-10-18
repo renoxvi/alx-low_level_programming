79fe35779afb63c92c57db7bab50920ad2811a77
renoxvi <vicreno08@gmail.com>
2022-10-18 10:15:08 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 0 to 100
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
