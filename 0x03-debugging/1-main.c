0d7db0ad255415a5437cc49bffff628240819b67
renoxvi <vicreno08@gmail.com>
2022-10-21 12:41:32 -0700

    Breaking the while loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -11,7 +12,6 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 /*
  * while (i < 10)
  * {
