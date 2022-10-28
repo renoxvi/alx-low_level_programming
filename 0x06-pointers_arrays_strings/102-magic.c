c57a587a8b5b35fd079b18f971b1489468e87cb8
renoxvi <vicreno08@gmail.com>
2022-10-28 05:46:27 -0700

    prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - magic happening
- * Return: Always 0
- */
 int main(void)
 {
 	int n;
@@ -12,7 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-       	/*
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
