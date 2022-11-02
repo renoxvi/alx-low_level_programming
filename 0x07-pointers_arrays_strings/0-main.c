f9cc82c6bf5d20f65a551805d14b4e148927bcbe
renoxvi <vicreno08@gmail.com>
2022-11-02 13:25:36 -0700

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -11,7 +11,7 @@
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
-	
+
 	i = 0;
 	while (i < size)
 	{
@@ -41,6 +41,6 @@ int main(void)
 	simple_print_buffer(buffer, 98);
 	_memset(buffer, 0x01, 95);
 	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);    
+	simple_print_buffer(buffer, 98);
 	return (0);
 }
