ab7618fadad9ff4c0d8917619a5d822d39c0e8d5
renoxvi <vicreno08@gmail.com>
2022-11-03 14:16:23 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-is_palindrome.c -o 100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,51 @@
 #include "main.h"
 
-int check_palindrome(char *s);
-
 /**
- * is_palindrome - Returns if a string is palindrome
- * @s: the string value to be checked
- * Return: integer value
- */
-int is_palindrome(char *s)
-{
-	if (*s == '0')
-		return (1);
-
-	return (check_palindrome(s));
-}
-
-/**
- * check_palindrome - Check if a string is palindrome
- * @s: the string value to be checked
- * Return: integer value
- */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-	{
-		return (0);
-	}
-
-	return (1);
-}
-
-/**
- * _strlen_recursion - Get the length of a string
- * @s: the string to get the length
- * Return: the string length
+ * _strlen_recursion - finds the length of a string
+ * @s: pointer to string
+ * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	s++;
 
-	return (_strlen_recursion(s) + 1);
+	return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * is_palind_recursive - checks if two chars of a string are equal
+ * @s: string to be checked
+ * @i: first index
+ * @j: last index
+ * Return: 1 if equal, else 0
+ */
+int is_palind_recursive(char *s, int i, int j)
+{
+	if (i == j)
+		return (1);
+
+	if (i == j - 1)
+		return (s[i] == s[j]);
+
+	if (s[i] != s[j])
+		return (0);
+
+	return (is_palind_recursive(s, i + 1, j - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to string
+ * Return: 1 if string is palindrome  else 0
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+
+	if (len == 0 || *s != s[len - 1])
+		return (0);
+	return (is_palind_recursive(s, 0, len - 1));
 }
