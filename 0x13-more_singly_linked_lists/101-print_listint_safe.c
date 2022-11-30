290edd5d09d0d0aa0862e5bacf3da342de4ac0ae
renoxvi <vicreno08@gmail.com>
2022-11-30 15:39:10 -0800

    correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,91 +1,27 @@
 #include "lists.h"
 
 /**
- * count_nodes_till_loop - count nodes to know now many unique nodes to print
- * @head: pointer to head pointer of linked list
- * Return: number of unique nodes in list before a loop
- */
-int count_nodes_till_loop(const listint_t *head)
-{
-	int count = 0;
-	const listint_t *turtle, *hare;
-
-	turtle = hare = head;
-	while (turtle != NULL && hare != NULL)
-	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-		count++;
-		if (turtle == hare)
-		{
-			turtle = head;
-			while (turtle != hare)
-			{
-				turtle = turtle->next;
-				hare = hare->next;
-				count++;
-			}
-			return (count);
-		}
-	}
-	return (0);
-}
-
-/**
- * loop - find if there's a loop in linked list
- * @head: pointer to head pointer of linked list
- * Return: 0 if no loop, 1 if loop
- */
-int loop(const listint_t *head)
-{
-	const listint_t *turtle, *hare;
-
-	turtle = hare = head;
-	while (turtle != NULL && hare != NULL)
-	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-		if (turtle == hare)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * print_listint_safe - prints list with addresses
- * @head: pointer to head pointer of linked list
- * Return: number of nodes in list, exit(98) if failed
+ * print_listint_safe - Listas enlazadas
+ * @head: Dado desde main
+ * Return: i
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0;
-	int loop_found;
-	size_t num_nodes = 0;
-	const listint_t *tmp;
+	size_t i = 0;
+	const listint_t *temp, *node;
 
-	if (head == NULL)
-		exit(98);
-
-	loop_found = loop(head);
-	if (loop_found == 1) /* print upto last node before loop if loop */
+	node = head;
+	while (node != NULL)
 	{
-		count = count_nodes_till_loop(head);
-		for (loop_found = 0; loop_found < count; loop_found++)
-		{
-			printf("[%p] %d\n", (void *)tmp, tmp->n);
-			num_nodes += 1;
-			tmp = tmp->next;
-		}
+		printf("[%p] %d\n", (void *)node, node->n);
+		temp = node;
+		node = node->next;
+		i++
+			if (temp <= node)
+			{
+				printf("-> [%p] %d\n", (void *)node, node->n);
+				break;
+			}
 	}
-	else if (loop_found == 0) /* print regularly upto NULL if no loop */
-	{
-		tmp = head;
-		while (tmp != NULL)
-		{
-			printf("[%p] %d\n", (void *)tmp, tmp->n);
-			num_nodes += 1;
-			tmp = tmp->next;
-		}
-	}
-	return (num_nodes);
+	return (i);
 }
--- 0x13-more_singly_linked_lists/102-main.c
+++ 0x13-more_singly_linked_lists/102-main.c
@@ -33,10 +33,10 @@ int main(void)
 	add_nodeint(&head, 3);
 	add_nodeint(&head, 4);
 	node->next = add_nodeint(&head, 98);
-	add_nodeint(&head, 402)
+	add_nodeint(&head, 402);
 	add_nodeint(&head, 1024);
 	print_listint_safe(head);
-	free_listint_safe(&head2);
+	print_listint_safe(&head2);
 	free_listint_safe(&head);
 	printf("%p, %p\n", (void *)head2, (void *)head);
 	return (0);
